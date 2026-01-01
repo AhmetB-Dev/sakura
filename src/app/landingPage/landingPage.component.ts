eaf2de4360e2f5bd658c0ba4d708fb290ad1595d
AhmetB-Dev <ahmet_ba@web.de>
2026-01-02 00:15:22 +0100

    feat: add landing page component with navbar and styles
    - Created LandingPageComponent with a section displaying the title and subtitle.
    - Added NavbarComponent for navigation with links and phone number.
    - Implemented SCSS styles for both components, including responsive design.
    - Integrated Noto Sans font for typography.
    - Updated app.component to include LandingPageComponent.

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'app-landingPage',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent],
+  template: `
+    <section>
+      <app-navbar></app-navbar>
+      <h1 class="fontNatoSans">SAKURA RAMEN</h1>
+      <h2 class="fontNatoSans">Best ramen in the town</h2>
+    </section>
+  `,
+  styleUrls: ['./landingPage.component.scss'],
+})
+export class LandingPageComponent {}
